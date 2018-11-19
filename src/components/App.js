5d490a8435a38bc8d6c8acd27f6d87c87666f176
omidshj <omid.shj@gmail.com>
2018-11-19 18:16:02 +0330

    initial

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar /> 
+        Appppppppp
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+
+  render() {
+    return <div>search bar</div>;
+  }
+}
+
+export default SearchBar;
